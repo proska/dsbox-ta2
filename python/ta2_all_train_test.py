2144834656dd246ae99eea1d5b9c1832a5cdd404
Serban Stan <stan@dsbox01.isi.edu>
2018-05-04 01:30:33 -0700

    added utility to run all datasets for training/testing

--- python/ta2_all_train_test.py
+++ python/ta2_all_train_test.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-
-"""
-Command Line Interface for running the DSBox TA2 Search
-"""
-
 from dsbox_dev_setup import path_setup
 path_setup()
 
@@ -24,36 +18,15 @@ from dsbox.planner.event_handler import PlannerEventHandler
 
 from pathlib import Path
 
+
 TIMEOUT = 25*60 # Timeout after 25 minutes
 
 DEBUG = 0
 LIB_DIRECTORY = os.path.dirname(os.path.realpath(__file__)) + "/library"
 
-def main(argv=None): # IGNORE:C0111
-    '''Command line options.'''
+DATA_DIR = '/nas/home/kyao/dsbox/data/datadrivendiscovery.org/data'
 
-    if argv is None:
-        argv = sys.argv
-    else:
-        sys.argv.extend(argv)
-
-    program_name = os.path.basename(sys.argv[0])
-    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
-    program_usage = '''%s
-USAGE
-ta2-search <search_config_file>
-''' % program_shortdesc
-
-    if len(sys.argv) < 2:
-        print(program_usage)
-        exit(1)
-
-    conf_file = sys.argv[1]
-    config = {}
-    with open(conf_file) as conf_data:
-        config = json.load(conf_data)
-        conf_data.close()
-
+def process_dataset(config):
     if "timeout" in config:
         # Timeout less 60 seconds, to give system chance to clean up
         TIMEOUT = int(config.get("timeout"))*60 - 60
@@ -72,7 +45,7 @@ ta2-search <search_config_file>
     signal.signal(signal.SIGTERM, write_results_and_exit)
     signal.signal(signal.SIGALRM, write_results_and_exit)
     signal.alarm(TIMEOUT)
-
+    
     # Load in data
     controller.initialize_training_data_from_config()
 
@@ -111,7 +84,7 @@ ta2-search <search_config_file>
 
     # for each pipeline compute its result on the test data by looking at the pickle files
     for ppln in controller.exec_pipelines:
-        print("Processing", ppln.primitives) 
+        print("Processing", ppln.primitives)
 
         primfile_base = config["temp_storage_root"] + "/models/" + ppln.id
         num_primitives = len(ppln.primitives)
@@ -137,7 +110,7 @@ ta2-search <search_config_file>
         assert last_primitive != None
 
         result = pandas.DataFrame(last_primitive.executables.produce(inputs=curr_testdata).value, index = curr_testdata.index, \
-               columns = [cn["colName"] for cn in data_manager.target_columns]) 
+               columns = [cn["colName"] for cn in data_manager.target_columns])
 
         # For each metric, compute the result
         for metric in problem.metrics:
@@ -153,13 +126,74 @@ ta2-search <search_config_file>
     print("Cleaning temp folder...")
 
     temp_output_folder = config["temp_storage_root"].rsplit("/", 1)[0]
-    shutil.rmtree(temp_output_folder) 
+    shutil.rmtree(temp_output_folder)
 
     print("Succesfully removed", temp_output_folder)
+
+
+def main(argv=None): # IGNORE:C0111
+    '''Command line options.'''
+
+    if argv is None:
+        argv = sys.argv
+    else:
+        sys.argv.extend(argv)
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-i", "--include", dest="include",type=str, nargs='+', default=[], help="list of families, algo types, or primitives to include")
+    parser.add_argument("-e", "--exclude", dest="exclude",type=str, nargs='+', default=[],  help="list of families, algo types, or primitives to exclude")
+    parser.add_argument("-d", "--dataset", dest="dataset",type=str, default= '/nas/home/kyao/dsbox/data/datadrivendiscovery.org/data/training_datasets/LL0/', help="dataset path")
+    parser.add_argument("-o", "--output", dest="output",type=str, default= 'outputs/', help='output dir')
+    args = parser.parse_args()
+
+    dataset_folder = args.dataset #sys.argv[1]
+    output_dir = args.output #sys.argv[2]
         
-                                    
+    include = args.include
+    exclude = args.exclude #sys.argv[4]
+    print(include)
+    print(exclude)
+
+    prnt = True
+    failures = []
+    count = 0
+    for folder, sub_dirs, _ in os.walk(dataset_folder):
+
+        dataset = folder.split('/')[-1]
+        if folder != dataset_folder and '.git' not in folder:
+            config = {}
+            del sub_dirs[:]
+            for i in range(20):
+                print()
+            print('RUNNING ', dataset)
+            config["problem_schema"] = os.path.join(dataset_folder, str(dataset), str(dataset+'_problem'), 'problemDoc.json')
+            config["problem_root"] = os.path.join(dataset_folder, str(dataset), str(dataset+'_problem'))
+            config["dataset_schema"] = os.path.join(dataset_folder, str(dataset), str(dataset+'_dataset'), 'datasetDoc.json')
+            config["training_data_root"] = os.path.join(dataset_folder, str(dataset), str(dataset+'_dataset'))
+            config["pipeline_logs_root"] =  os.path.join(os.getcwd(), output_dir, str(dataset),'logs')
+            config["executables_root"] =  os.path.join(os.getcwd(), output_dir, str(dataset),'executables')  
+            config["temp_storage_root"] =  os.path.join(os.getcwd(), output_dir, str(dataset),'temp')
+            config["timeout"] = TIMEOUT
+            config["include"] = include
+            config["exclude"] = exclude
+            # don't change timeout, cpus, ram?
+
+            try:
+                process_dataset(config)
+                count = count + 1
+            except:
+                failures.append(dataset)
+                continue
+
+    print("Completed Running ", count, " Pipelines")
+    print("Failed on datasets:")
+    for fail in failures:
+        print(fail)
+
+
 if __name__ == "__main__":
     if DEBUG:
         sys.argv.append("-h")
         sys.argv.append("-v")
     sys.exit(main())
+    #main()
