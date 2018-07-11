4ab00d5f637c7cdd30023a394914cf53ab568929
Serban Stan <stan@dsbox01.isi.edu>
2018-07-10 21:52:02 -0700

    timeout added

--- python/train_test.py
+++ python/train_test.py
@@ -2,11 +2,28 @@ import os
 from pathlib import Path
 
 import subprocess
+from threading import Timer
 
 home = str(Path.home())
-config_dir = home + "/dsbox/runs2/config-seed/"
+config_dir = home + "/dsbox/runs2/config-ll0/"
 
+# https://www.blog.pythonlibrary.org/2016/05/17/python-101-how-to-timeout-a-subprocess/
+kill = lambda process: process.kill()
+timers = []
 for conf in os.listdir(config_dir):
 	command = "python ta2-search " + config_dir + conf
 	print(command)
-	subprocess.Popen(command, shell=True)
+
+	curr_proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	curr_timer = Timer(60, kill, [curr_proc])
+
+	timers.append(curr_timer)
+
+for timer in timers:
+	timer.start()
+
+
+# for conf in os.listdir(config_dir):
+# 	command = "python ta2-search " + config_dir + conf
+# 	print(command)
+# 	subprocess.Popen(command, shell=True)
